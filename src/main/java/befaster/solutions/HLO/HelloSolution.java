d099ad6d17a283f129a203aea89f11ce60ca839c
sbx_user1051 <sbx_user1051@169.254.172.117>
2020-08-17 15:32:08 +0000

    Mon Aug 17 15:32:08 UTC 2020

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -4,8 +4,7 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class HelloSolution {
     public String hello(String friendName) {
-        System.out.print(friendName);
-      return friendName;
+      return "Hello " + friendName + "!";
+
     }
 }
-
