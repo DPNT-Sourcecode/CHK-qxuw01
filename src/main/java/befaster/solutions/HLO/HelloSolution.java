7c60f98ddf2ce172c157fdfc0a6d6a7d0c1e7cb3
sbx_user1051 <sbx_user1051@169.254.172.117>
2020-08-17 15:45:01 +0000

    Mon Aug 17 15:45:01 UTC 2020

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -3,10 +3,11 @@ package befaster.solutions.HLO;
 
 public class HelloSolution {
     public String hello(String friendName) {
-      return String.format("Hello, %s!", friendName);
+      return "Hello, " + friendName + "!";
 
     }
 }
+
 
 
 
