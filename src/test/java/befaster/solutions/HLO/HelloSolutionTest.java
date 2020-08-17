8acfe9d032ede6876aafcbdb25e30c2586265311
sbx_user1051 <sbx_user1051@169.254.172.117>
2020-08-17 15:18:43 +0000

    Mon Aug 17 15:18:43 UTC 2020

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,5 +1,23 @@
-import junit.framework.TestCase;
+package befaster.solutions.HLO;
 
-public class HelloSolutionTest extends TestCase {
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloSolutionTest {
+  private HelloSolution helloSolution;
+
+  @Before
+  public void setUp(){
+    helloSolution = new HelloSolution();
+  }
+
+  @Test
+  public void testHello(){
+    assertThat(helloSolution.hello("world"), is("world"));
+
+  }
 
 }
