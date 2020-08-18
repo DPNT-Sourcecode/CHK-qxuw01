8d612eedb4ede8dda8c1cb168fec6c8dd9dd21a6
sbx_user1051 <sbx_user1051@169.254.242.189>
2020-08-18 15:32:48 +0000

    Tue Aug 18 15:32:48 UTC 2020

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,5 +1,10 @@
+package befaster.solutions.CHK;
+
 import junit.framework.TestCase;
 
 public class CheckoutSolutionTest extends TestCase {
 
+  public void testCheckout() {
+    CheckoutSolution checkoutSolution = new CheckoutSolution();
+  }
 }
